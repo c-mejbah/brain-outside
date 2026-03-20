a9f625ff33480b6a72453ad85ccf0d3d491a45d4
Mejbah <meherabmejbah@gmail.com>
2026-03-20 16:21:51 +0600

    Update en_to_ban.c

--- en_to_ban.c
+++ en_to_ban.c
@@ -3,12 +3,12 @@ winding = ঘুর
 
 devoted to = অনুগত , নিবেদিত 
 daunting = ভয়ঙ্কর 
-
-
-
-
-
-
+candidates  = ভয়ঙ্কর
+significant = উল্লেখযোগ্য 
+auction = bidding 
+construct = নির্মাণ
+bent = বাঁকানো
+nuts and bolts = important
 
 
 
